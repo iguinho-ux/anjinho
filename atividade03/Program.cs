feeeb68e4397fae375ee1017e2eb55082ac367c6
iguinho-ux <igormachadosr@gmail.com>
2025-04-24 09:07:03 -0300

    atidd03

---
+++ atividade03/Program.cs
@@ -0,0 +1,8 @@
+﻿
+string usuario = "admin";
+if (usuario == "admin" || usuario == "supervisor")
+{
+    Console.WriteLine("Acesso concedido");
+}
+
+// Se o usuário for "admin" ou "supervisor", exiba "Acesso concedido".
