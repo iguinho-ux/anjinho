6d06404c3696a238f5dd345c77f8f878d5dfaa43
iguinho-ux <igormachadosr@gmail.com>
2025-04-24 10:24:09 -0300

    atvdd7

---
+++ atividade7/Program.cs
@@ -0,0 +1,10 @@
+﻿bool cadastrado = true;
+bool cupomValido = false;
+bool jaUsouCupom = false;
+
+if ((cadastrado  && cupomValido) && !jaUsouCupom)
+
+{
+
+    Console.WriteLine(cupomValido = true);
+}
