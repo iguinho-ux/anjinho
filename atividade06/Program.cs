6a1443ac8b6e87f6dd18e5f238f69a65f2da3211
iguinho-ux <igormachadosr@gmail.com>
2025-04-24 09:40:07 -0300

    atvdd6

---
+++ atividade06/Program.cs
@@ -0,0 +1,10 @@
+﻿bool temIngresso = true;
+bool documentoValido = false;
+if (temIngresso && documentoValido)
+{
+    Console.WriteLine("Entrada permitida");
+}
+else
+{
+    Console.WriteLine("Entrada negada");
+}
